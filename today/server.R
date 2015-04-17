16b5bc135cb9cc8ab6f6e50ddd3889c0e6528491
Tim Coote <tim+github.com@coote.org>
2015-04-17 15:03:22 +0100

    working version of today

--- today/server.R
+++ today/server.R
@@ -1,35 +1,27 @@
 library (ggvis)
 library (dplyr)
 
-
 cnames = c ("date", "EV", "EA", "EW", "WV", "WA", "WW", "OV", "OA", "OW", "OHz", "Efficiency", "Temp1", "Temp2", "E_Day", "E_Week", "dunno", "E_month", "E_Year", "E Tot1", "E Tot2", "Response")
 
 shinyServer(function(input, output, session) {
-#    vals = read.table ("../../logging/output", header = F, col.names = cnames, fill=TRUE)
     vals = reactiveFileReader (1000, session, "../../logging/output", read.table, header = F, col.names = cnames, fill=TRUE)
     
-    
-    #t4 = mrv (today - 3, 1)
-    
-    # remove rows with missing values (na.omit)
-    t1 = mutate (na.omit (vals), ts = as.POSIXct (date, format= "%Y%m%d-%H:%M:%S"), year = as.POSIXlt (ts)$year, month = as.POSIXlt (ts)$mon, day = as.POSIXlt (ts)$yday + 1, hour = as.POSIXlt(ts)$hour)
-    readingfunc = function (tvals) {select (tvals, ts, year, month, day, hour, EW, WW, OW, E_Day, E_month, E_Year, Efficiency)}
-#    t = select (t1, ts, year, month, day, hour, EW, WW, OW, E_Day, E_month, E_Year, Efficiency)
-    thisdata = readingfunc (t1)
-#    thisdata = function () { return (t)}
-#    thisdata = reactive ({ return (t)})
+# remove rows with missing values (na.omit)
+    t1 = reactive ({ mutate (na.omit (vals()), ts = as.POSIXct (date, format= "%Y%m%d-%H:%M:%S"), year = as.POSIXlt (ts)$year, month = as.POSIXlt (ts)$mon, day = as.POSIXlt (ts)$yday + 1, hour = as.POSIXlt(ts)$hour) })
+#    readingfunc = function (tvals) {select (tvals, ts, year, month, day, hour, EW, WW, OW, E_Day, E_month, E_Year, Efficiency)}
+    thisdata = reactive ({select (t1(), ts, year, month, day, hour, EW, WW, OW, E_Day, E_month, E_Year, Efficiency)})
+
     cat ("first\n", file =stderr())    
-    cat (class(thisdata), file =stderr())    
+#    cat (class(t()), file =stderr())    
     cat ("second\n", file =stderr())    
-#    ts = as.POSIXlt (t1$date, format= "%Y%m%d-%H:%M:%S")
-    #p = layer_points(ggvis (readingfunc(t1), ~ts, ~E_Day, stroke=~year, fill=~year), size =5 )
-    p = layer_points(ggvis (thisdata, ~ts, ~E_Day), size =5 )
+#    p = layer_points(ggvis (readingfunc(t1), ~ts, ~E_Day, stroke=~year, fill=~year), size =5 )
+    p =  layer_points(ggvis (thisdata, ~ts, ~E_Day), size =5 )
 
-    mymax = function (x) {return (max (x))}
-    f = function (j) {return (j[1,])}
-#    f1 = f (readingfunc(t1))
-#    cat (f (thisdata), file = stderr ())
+    dom=reactive ({c (0, max (thisdata()$E_Day) + 1) })
 
-    p %>%  scale_numeric("y", domain = c(0, 4)) %>% set_options (renderer="canvas") %>% bind_shiny("ggvis", "ggvis_ui")
+    p  %>%  scale_numeric("y", domain=dom) %>% set_options (renderer="canvas") %>% bind_shiny("ggvis", "ggvis_ui")
 
+#    output$theWords = renderText ({paste ("output today", tail (thisdata ()$E_Day, 1)) })
+    output$theWords = renderDataTable ({ tail (thisdata ()[,c(6,7,8)], 2) }, options = list(paging=FALSE, searching=FALSE, pageLength = 2))
+#    output$theWords = renderUI ({em (paste ("current output", tail (thisdata ()$OW, 1))) })
 })
--- today/ui.R
+++ today/ui.R
@@ -1,11 +1,15 @@
-
 library (ggvis)
 library (dplyr)
 
 
-
-shinyUI(bootstrapPage(
+shinyUI(pageWithSidebar(
+  headerPanel ("Current output"),
+  sidebarPanel(
+    dataTableOutput ("theWords")
+#    textOutput ("theWords")
+  ),
+  mainPanel(
     uiOutput("ggvis_ui"),
     ggvisOutput("ggvis")
   )
-)
+))
