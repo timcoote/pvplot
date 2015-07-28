a3b5e6a40a65fb1e3001d9ffe60760e471b618c0
Tim Coote <tim+github.com@coote.org>
2015-07-28 20:28:37 +0100

    this is broken - wrong x axis scale

--- garmin/server.R
+++ garmin/server.R
@@ -3,23 +3,23 @@ library (ggvis)
 library (dplyr)
 library (plyr)
 
-cnames = c ("date", "EV", "EA", "EW", "WV", "WA", "WW", "OV", "OA", "OW", "OHz", "Efficiency", "Temp1", "Temp2", "E_Day", "E_Week", "dunno", "E_month", "E_Year", "E Tot1", "E Tot2", "Response")
 
 shinyServer(function(input, output, session) {
-    vals = reactiveFileReader (1000, session, "hearts", read.table, header = F, col.names = c("date", "rate"), fill=TRUE)
+    vals = reactiveFileReader (1000, session, "hearts2", read.table, header = F, col.names = c("date", "heartrate", "speed"), fill=TRUE)
     
 # remove rows with missing values (na.omit)
-    t1 = reactive ({ mutate (na.omit (vals()), ts = as.POSIXct (date, origin= "1970-1-1"), year = as.POSIXlt (ts)$year, month = as.POSIXlt (ts)$mon, day = as.POSIXlt (ts)$yday + 1, hour = as.POSIXlt(ts)$hour) })
+    t1 = reactive ({ mutate (na.omit (vals()), ts = as.POSIXct (date, origin= "1970-1-1")) })
 
-    thisdata = reactive ({select (t1(), ts, year, month, day, hour, rate)})
+    thisdata = reactive ({select (t1(), ts, heartrate, speed)})
 
-    cat ("first\n", file =stderr())    
-#    cat (class(t()), file =stderr())    
-    cat ("second\n", file =stderr())    
-    cumE =  layer_points(ggvis (thisdata, ~ts, ~rate), size =0.1 )
+    domE=reactive ({c (0, max (thisdata()$heartrate) + 1) })
 
-    domE=reactive ({c (0, max (thisdata()$rate) + 1) })
-    cumE  %>%  scale_numeric("y", domain=domE) %>% set_options (renderer="canvas") %>% bind_shiny("cumsum", "cumsum_ui")
+    heartplot =  layer_points(ggvis (thisdata, x = ~ts), y = ~heartrate, size =0.1 ) %>% add_axis ("y", orient="left", title="Heart rate") %>%
+      scale_numeric ("y", domain=domE) #%>%
+#      add_axis ("y", 'spd', orient="right", title= "Speed ()", grid=F) %>%
+#      layer_points (prop ('y', ~speed, scale='spd'), stroke :="red", size=0.1) # %>% scale_numeric ("x", domain = c(min(ts()), max(ts())))
+
+    heartplot  %>%  set_options (renderer="canvas") %>% bind_shiny("cumsum", "cumsum_ui")
 
     output$theWords = DT::renderDataTable ({ tail (thisdata ()[,c(9, 6,7,8)], 2) }, options = list(paging=FALSE, searching=FALSE, pageLength = 2), rownames=FALSE)
 })
