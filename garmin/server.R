1d8496f5378ee3b9bf8fd599b34e70d931d2906a
Tim Coote <tim+github.com@coote.org>
2015-07-28 20:40:39 +0100

    works. uses a hearts2 created on mac. by python script

--- garmin/server.R
+++ garmin/server.R
@@ -8,16 +8,16 @@ shinyServer(function(input, output, session) {
     vals = reactiveFileReader (1000, session, "hearts2", read.table, header = F, col.names = c("date", "heartrate", "speed"), fill=TRUE)
     
 # remove rows with missing values (na.omit)
-    t1 = reactive ({ mutate (na.omit (vals()), ts = as.POSIXct (date, origin= "1970-1-1")) })
+    t1 = reactive ({ mutate (na.omit (vals()), ts = as.POSIXct (date/1000, origin= "1970-1-1")) })
 
     thisdata = reactive ({select (t1(), ts, heartrate, speed)})
 
     domE=reactive ({c (0, max (thisdata()$heartrate) + 1) })
 
     heartplot =  layer_points(ggvis (thisdata, x = ~ts), y = ~heartrate, size =0.1 ) %>% add_axis ("y", orient="left", title="Heart rate") %>%
-      scale_numeric ("y", domain=domE) #%>%
-#      add_axis ("y", 'spd', orient="right", title= "Speed ()", grid=F) %>%
-#      layer_points (prop ('y', ~speed, scale='spd'), stroke :="red", size=0.1) # %>% scale_numeric ("x", domain = c(min(ts()), max(ts())))
+      scale_numeric ("y", domain=domE) %>%
+      add_axis ("y", 'spd', orient="right", title= "Speed (m/s)", grid=F, properties = axis_props (axis = list(stroke= "red"))) %>%
+      layer_points (prop ('y', ~speed, scale='spd'), stroke :="red", fill := "red", size=0.1) # %>% scale_numeric ("x", domain = c(min(ts()), max(ts())))
 
     heartplot  %>%  set_options (renderer="canvas") %>% bind_shiny("cumsum", "cumsum_ui")
 
